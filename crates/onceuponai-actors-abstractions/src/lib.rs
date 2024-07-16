1c6a813da44cc87bc628b389bb4ca8b118640b24
qooba <dev@qooba.net>
2024-07-16 02:12:53 +0200

    redesign

--- crates/onceuponai-actors-abstractions/src/lib.rs
+++ crates/onceuponai-actors-abstractions/src/lib.rs
@@ -1,6 +1,8 @@
+use actix_telepathy::RemoteAddr;
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::net::SocketAddr;
 use uuid::Uuid;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -13,11 +15,14 @@ pub struct ActorMetadata {
 
 pub trait ActorActions {
     fn actor_id(&self) -> &str;
-    fn metadata(&self) -> ActorMetadata;
-    fn kind(&self) -> String;
-    fn start(&self);
-    // fn invoke(&self, uuid: Uuid, request: ActorInvokeRequest) -> Result<ActorInvokeResponse>;
     fn is_main(&self) -> bool;
+    fn kind(&self) -> String;
+    fn metadata(&self) -> ActorMetadata;
+    fn own_addr(&self) -> Result<SocketAddr>;
+    fn remote_addr(&self) -> Result<RemoteAddr>;
+    fn seed_addr(&self) -> Result<SocketAddr>;
+    fn start(&self) -> Result<()>;
+    // fn invoke(&self, uuid: Uuid, request: ActorInvokeRequest) -> Result<ActorInvokeResponse>;
 }
 
 struct ActorConfig<T> {
@@ -26,7 +31,32 @@ struct ActorConfig<T> {
 }
 
 impl<T> ActorConfig<T> {
+    fn actor_id(&self) -> &str {
+        todo!()
+    }
+
     fn metadata(&self) -> ActorMetadata {
         self.metadata.clone()
+    }
+
+    fn own_addr(&self) -> Result<SocketAddr> {
+        let socket_addr: SocketAddr = self.metadata().actor_host.parse::<SocketAddr>()?;
+        Ok(socket_addr)
+    }
+
+    fn remote_addr(&self) -> Result<RemoteAddr> {
+        let socket_addr: SocketAddr = self.own_addr()?;
+        let remote_addr = RemoteAddr::new_from_id(socket_addr, self.actor_id());
+        Ok(remote_addr)
+    }
+
+    fn seed_addr(&self) -> Result<SocketAddr> {
+        let socket_addr = self
+            .metadata()
+            .actor_seed
+            .clone()
+            .expect("SEED REQUIRED")
+            .parse::<SocketAddr>()?;
+        Ok(socket_addr)
     }
 }
