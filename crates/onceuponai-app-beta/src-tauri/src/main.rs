de9abfd5f018a16be9dcf26b22e65e13cd460c5b
qooba <dev@qooba.net>
2024-07-21 15:11:44 +0200

    migrate to tauri 2

--- crates/onceuponai-app-beta/src-tauri/src/main.rs
+++ crates/onceuponai-app-beta/src-tauri/src/main.rs
@@ -1,16 +1,44 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use server::{TauriAppConfig, TauriAppState};
+use std::sync::{Arc, Mutex};
+use std::thread;
+use tauri::{AppHandle, Manager, State};
+
+pub mod server;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    format!("Hello, {}! You've been greeted from Rust1!", name)
+}
+
+#[tauri::command]
+fn config(handle: AppHandle) -> TauriAppConfig {
+    let state: State<TauriAppState> = handle.state();
+    let config = state.config.lock().unwrap();
+    TauriAppConfig {
+        personal_token: config.personal_token.clone(),
+        base_url: config.base_url.clone(),
+    }
 }
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let config = Arc::new(Mutex::new(TauriAppConfig::default()));
+            let shared_config = Arc::clone(&config);
+            app.manage(TauriAppState { config });
+            thread::spawn(move || {
+                server::init(shared_config).unwrap();
+            });
+
+            Ok(())
+        })
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_http::init())
+        .invoke_handler(tauri::generate_handler![greet, config])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- crates/onceuponai-app-beta/src/App.vue
+++ crates/onceuponai-app-beta/src/App.vue
@@ -1,30 +1,18 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Greet from "./components/Greet.vue";
+import Navigation from "./components/Navigation.vue";
+
 </script>
 
 <template>
-  <div class="container">
-    <h1>Welcome to Tauri 1!</h1>
-
-
-    <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
-
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
-
-    <Greet />
-  </div>
+  <v-app>
+    <Navigation />
+    <v-main>
+      <router-view />
+      <router-view name="footer" />
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped>
@@ -112,6 +100,7 @@ button {
 button:hover {
   border-color: #396cd8;
 }
+
 button:active {
   border-color: #396cd8;
   background-color: #e8e8e8;
@@ -141,9 +130,14 @@ button {
     color: #ffffff;
     background-color: #0f0f0f98;
   }
+
   button:active {
     background-color: #0f0f0f69;
   }
 }
-
 </style>
+
+
+
+
+
