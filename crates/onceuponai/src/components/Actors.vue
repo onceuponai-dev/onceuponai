963ee118cadef79c0663f5489514af39e9fb88de
qooba <dev@qooba.net>
2024-10-28 01:39:45 +0100

    change actors

--- crates/onceuponai/src/components/Actors.vue
+++ crates/onceuponai/src/components/Actors.vue
@@ -495,7 +495,7 @@ watch(spawnSelectedSearch, (newValue) => {
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="primary" @click="remoteSpawnDialog = false">Close</v-btn>
+          <v-btn color="primary" @click="remoteSpawnDialog = false"><b>Close</b></v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
